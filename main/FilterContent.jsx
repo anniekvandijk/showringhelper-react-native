c7c21141bb90d7083711ddfea4ec84855f798f85
anniekvandijk <anniek@animundo.nl>
2019-08-17 23:24:44 +0200

    checkBoxes

--- main/FilterContent.jsx
+++ main/FilterContent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import {
-  Content, Button, Text, Card, CardItem, Left, Right, Icon
+  Spinner, ListItem, Content, Button, Text, Card, CheckBox, CardItem, Left, Body, Right, Icon
 } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { useShowContext } from '../context/showContext';
@@ -13,10 +13,46 @@ const style = StyleSheet.create({
   }
 });
 
-function FilterContent({ navigation }) {
+function FilterContent() {
   const [t] = useTranslation();
   const shows = useShowContext();
-  const [filter, setFilter] = useState([]);
+  const [showList, setShowList] = useState(null);
+  const [showFilter, setShowFilter] = useState([]);
+
+  useEffect(() => {
+    setShowList(shows);
+  }, [shows]);
+
+  useEffect(() => {
+    console.log(showFilter);
+  }, [showFilter]);
+
+  function isFiltered(id) {
+    if (showFilter.indexOf(id) > -1) {
+      return true;
+    }
+    return false;
+  }
+
+  function handleChange(id, bool) {
+    console.log(id, bool);
+    if (bool) {
+      setShowFilter([...showFilter, id]);
+    } else {
+      setShowFilter(showFilter.filter(x => x !== id));
+    }
+  }
+
+  if (!showList) {
+    return (
+      <>
+        <Spinner />
+        <Text style={style.spinnerText}>
+          {t('spinner')}
+        </Text>
+      </>
+    );
+  }  
 
   return (
     <>
@@ -27,6 +63,20 @@ function FilterContent({ navigation }) {
               <Text>Filter comming soon ...</Text>
             </Left>
           </CardItem>
+          {showList && showList.map(show => (
+            <CardItem bordered key={show.id}>
+              <Left>
+                <CheckBox
+                  checked={isFiltered(show.id)}
+                  onPress={() => handleChange(show.id, !isFiltered(show.id))}
+                />
+              </Left>
+              <Body>
+                <Text>{show.name}</Text>
+              </Body>
+            </CardItem>
+          )
+        )}
         </Card>
       </Content>
     </>
