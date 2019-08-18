8b41adb45939652ef6cd1d2f82cd505a27c4742d
anniekvandijk <anniek@animundo.nl>
2019-08-18 13:24:04 +0200

    lacalisation

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-//import * as Localization from 'expo-localization';
+import * as Localization from 'expo-localization';
 import translationEn from './locales/en/translation.json';
 import translationNl from './locales/nl/translation.json';
 
@@ -17,8 +17,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    //lng: Localization.locale,
-    lng: 'en',
+    lng: Localization.locale,
     fallbackLng: 'en',
     keySeparator: '.', // we do not use keys in form messages.welcome
     interpolation: {
