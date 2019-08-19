ada51791d25a5fef9ff0cca5f9fcc3b421ffd81a
anniekvandijk <anniek@animundo.nl>
2019-08-19 23:58:51 +0200

    working language switch

--- main/SettingsDetail.jsx
+++ main/SettingsDetail.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StyleSheet } from 'react-native';
 import {
-  Content, Text, Card, CardItem, Body
+  Content, Text, Card, CardItem, Body, Picker, Right, Left, Icon, Header, Button, Title
 } from 'native-base';
 
 const style = StyleSheet.create({
@@ -12,15 +12,62 @@ const style = StyleSheet.create({
 });
 
 function SettingsDetail() {
-  const [t] = useTranslation();
+  const [t, i18n] = useTranslation();
+  const [language, setLanguage] = useState(i18n.language.split('-')[0]);
+
+  function handleChangeLanguage(value) {
+    setLanguage(value);
+  }
+
+  useEffect(() => {
+    if (!language) {
+      setLanguage(i18n.language.split('-')[0]);
+      // store
+    }
+    console.log(language);
+    i18n.changeLanguage(language);
+  }, [language]);
+
   return (
     <>
       <Content padder style={style.content}>
         <Card>
           <CardItem bordered>
-            <Body>
-              <Text>Save User preferences</Text>
-            </Body>
+            <Left>
+              <Text>
+                {t('pages.settingsDetail.language.text')}
+              </Text>
+            </Left>
+            <Right>
+              <Picker
+                renderHeader={backAction => (
+                  <Header>
+                    <Left>
+                      <Button transparent onPress={backAction}>
+                        <Icon name="arrow-back" />
+                      </Button>
+                    </Left>
+                    <Body style={{ flex: 3 }}>
+                      <Title>
+                        {t('pages.settingsDetail.language.headerPlaceholder')}
+                      </Title>
+                    </Body>
+                    <Right />
+                  </Header>
+                )
+                }
+                mode="dropdown"
+                iosHeader={t('pages.settingsDetail.language.headerPlaceholder')}
+                iosIcon={<Icon name="arrow-down" />}
+                placeholder={t('pages.settingsDetail.language.headerPlaceholder')}
+                style={{ width: undefined }}
+                selectedValue={language}
+                onValueChange={value => handleChangeLanguage(value)}
+              >
+                <Picker.Item label="Nederlands" value="nl" />
+                <Picker.Item label="English" value="en" />
+              </Picker>
+            </Right>
           </CardItem>
         </Card>
       </Content>
