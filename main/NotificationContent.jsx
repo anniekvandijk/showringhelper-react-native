56802edbd8a42736a1e97eda81818e86547426d0
anniekvandijk <anniek@animundo.nl>
2019-11-05 18:39:14 +0100

    picker ios fix

--- main/NotificationContent.jsx
+++ main/NotificationContent.jsx
@@ -179,6 +179,8 @@ function NotificationContent() {
           </Left>
         </CardItem>
         <CardItem bordered>
+          {Platform.OS === 'android'
+          ? (
           <Picker
             renderHeader={backAction => (
               <Header>
@@ -206,14 +208,10 @@ function NotificationContent() {
             selectedValue={show}
             onValueChange={value => handleChangeShow(value)}
           >
-            {Platform.OS === 'android'
-              && (
-                <Picker.Item
-                  label={t('pages.notificationContent.selectShow')}
-                  value="-1"
-                />
-              )
-            }
+            <Picker.Item
+              label={t('pages.notificationContent.selectShow')}
+              value="-1"
+            />
             {showList && showList.map(showItem => (
               <Picker.Item
                 label={showItem.name}
@@ -222,6 +220,45 @@ function NotificationContent() {
               />
             ))}
           </Picker>
+            )
+        : (
+          <Picker
+            renderHeader={backAction => (
+              <Header>
+                <Left>
+                  <Button transparent onPress={backAction}>
+                    <Icon name="arrow-back" />
+                  </Button>
+                </Left>
+                <Body style={{ flex: 3 }}>
+                  <Title>
+                    {t('pages.notificationContent.showPickerPlaceholder')}
+                  </Title>
+                </Body>
+                <Right />
+              </Header>
+            )
+            }
+            mode="dropdown"
+            iosHeader={t('pages.notificationContent.showPickerPlaceholder')}
+            iosIcon={<Icon name="arrow-down" />}
+            placeholder={t('pages.notificationContent.showPickerPlaceholder')}
+            placeholderStyle={{ color: '#000' }}
+            style={{ width: '85%' }}
+            itemTextStyle={{ color: '#000000' }}
+            selectedValue={show}
+            onValueChange={value => handleChangeShow(value)}
+          >
+            {showList && showList.map(showItem => (
+              <Picker.Item
+                label={showItem.name}
+                key={Math.random().toString(36).substring(7)}
+                value={showItem}
+              />
+            ))}
+          </Picker>
+          )
+        }
         </CardItem>
         {show
           && (
