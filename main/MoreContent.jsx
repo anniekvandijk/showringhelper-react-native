a7e61c45cf6d9c49f99562c6d59274d28b4e6bc6
anniekvandijk <anniek@animundo.nl>
2019-11-06 22:19:21 +0100

    version 17

--- main/MoreContent.jsx
+++ main/MoreContent.jsx
@@ -30,7 +30,7 @@ function MoreContent({ navigation }) {
           </CardItem>
           <CardItem bordered>
             <Body>
-              <Text>Showring helper {t('pages.moreContent.versionText')} 0.16.0 ©Animundo 2019</Text>
+              <Text>Showring helper {t('pages.moreContent.versionText')} 0.17.0 ©Animundo 2019</Text>
             </Body>
           </CardItem>
           <CardItem bordered>
