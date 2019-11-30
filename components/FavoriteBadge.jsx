5bf69d3497f1d95510b8c37715a9653018dd0b34
anniekvandijk <anniek@animundo.nl>
2019-11-30 16:36:15 +0100

    star

--- components/FavoriteBadge.jsx
+++ components/FavoriteBadge.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Icon, Badge, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
+import { Icon, Badge, Text, Button } from 'native-base';
+import { Platform, StyleSheet } from 'react-native';
 
 const style = StyleSheet.create({
   icon: {
+    color: '#fad201',
+    backgroundColor: '#ffffff',
+    marginLeft: 0,
+    marginRight: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    fontSize: 20
+  },
+  button: {
+    //backgroundColor: '#197b30',
+    backgroundColor: '#ffffff',
+    borderColor: '#ffffff',
+    position: 'relative',
+    margin: 0,
+    paddingLeft: -30,
+  },
+  iconIos: {
     color: '#fad201',
     marginLeft: 0,
     paddingLeft: 0,
     paddingRight: 0,
     fontSize: 20
   },
-  badge: {
-    //backgroundColor: '#197b30',
+  badgeIos: {
     backgroundColor: 'transparent',
     position: 'relative',
     marginLeft: -35,
@@ -19,20 +35,38 @@ const style = StyleSheet.create({
   }
 });
 
-
 function FavoriteBadge() {
+  if (Platform.OS === 'android') {
+    return (
+      <Button
+        rounded
+        small
+        bordered
+        transparent
+        style={style.button}
+      >
+        <Text>
+          <Icon
+            style={style.icon}
+            name="star"
+          />
+        </Text>
+      </Button>
+    );
+  }
   return (
     <Badge
-      style={style.badge}
+      style={style.badgeIos}
     >
       <Text>
         <Icon
-          style={style.icon}
+          style={style.iconIos}
           name="star"
         />
       </Text>
     </Badge>
   );
 }
+
 
 export default FavoriteBadge;
