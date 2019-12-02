e4a9b48ed22bad1bd762ae4e05ac2c073ae2bd44
anniekvandijk <anniek@animundo.nl>
2019-12-02 14:45:00 +0100

    no show styling

--- components/NoShowCard.jsx
+++ components/NoShowCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, Card, CardItem, Body, H1 } from 'native-base';
+import { Text, Card, CardItem, Body } from 'native-base';
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  header: {
+    backgroundColor: '#197b30'
+  },
+  headerTitle: {
+    color: '#ffffff',
+    fontWeight: 'bold'
+  }
+});
 
 function NoShowCard() {
   const [t] = useTranslation();
   return (
     <Card>
-      <CardItem header bordered>
-        <Text>{t('components.noShowCard.header')}</Text>
+      <CardItem header bordered style={style.header}>
+        <Text style={style.headerTitle}>{t('components.noShowCard.header')}</Text>
       </CardItem>
       <CardItem bordered>
         <Body>
